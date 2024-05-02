af20e4c33b7d69741ee0f07a5e2b91c71d12e9a3
zeynep zıvalı <148189146+zeybit@users.noreply.github.com>
2024-05-02 11:24:14 +0300

    Add files via upload

---
+++ star,cark/sketch.js
@@ -0,0 +1,47 @@
+let angle = 0.0;
+let jitter = 0.0;
+function setup() {
+  createCanvas(720, 400);
+}
+
+function draw() {
+  background(102);
+if (second() % 2 === 0) {
+    jitter = random(-0.1, 0.1);
+  }
+  
+  angle = angle + jitter;
+  let c = cos(angle);
+  push();
+  translate(width * 0.2, height * 0.5);
+  rotate(c);
+  star(0, 0, 5, 70, 3);
+  pop();
+
+  push();
+  translate(width * 0.5, height * 0.5);
+  rotate(c);
+  star(0, 0, 80, 100, 40);
+  pop();
+
+  push();
+  translate(width * 0.8, height * 0.5);
+  rotate(c);
+  star(0, 0, 30, 70, 5);
+  pop();
+}
+
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
