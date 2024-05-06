ded47e94383b35fab393a0d1d5c14d71a59c8f8b
zeynep zıvalı <148189146+zeybit@users.noreply.github.com>
2024-05-06 11:53:01 +0300

    Add files via upload

---
+++ framecount_normalmaterialrotate_/sketch.js
@@ -0,0 +1,65 @@
+
+function setup() {
+  createCanvas(710, 400, WEBGL);
+}
+
+function draw() {
+  background(250);
+
+  normalMaterial();
+  push();
+  translate(-240, -100, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  plane(70);
+  pop();
+
+  push();
+  translate(0, -100, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(70, 70, 70);
+  pop();
+
+  push();
+  translate(240, -100, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cylinder(70, 70);
+  pop();
+
+  push();
+  translate(-250, 100, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cone(50, 70);
+  pop();
+
+  push();
+  translate(-75, 100, 0);  
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(50, 20);
+  pop();
+
+  push();
+  translate(100, 100, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  sphere(50);
+  pop();
+
+  push();
+  translate(275, 100, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  ellipsoid(30, 40, 40);
+  pop();
+}
